332bfbb97bef3bbdc3201ed5aaceb72b99fd1eca
Exzhnik <exzhnik@gmail.com>
2018-11-11 14:27:00 +0400

    hw2 first

---
+++ app/src/main/java/com/example/exzhnik/cutaway/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.exzhnik.cutaway;
+
+import android.app.Activity;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Bundle savedInstanceState1 = savedInstanceState;
+        super.onCreate(savedInstanceState1);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
